d6239cc47f879b92ba9d27e51871d247f53f5585
alextrukhin <141617261+alextrukhin@users.noreply.github.com>
2026-06-11 21:39:52 +0200

    fix permissions

--- packages/website/server/api/dishes/[id]/photo.get.ts
+++ packages/website/server/api/dishes/[id]/photo.get.ts
@@ -1,18 +1,63 @@
 import { readClient } from "prismaclient";
 import { getObject } from "../../../services/s3/local";
-import { getEventCatererStaff } from "../../../trpc/trpc";
+import {
+	getEventCatererStaff,
+	getEventOrgStaff,
+	getEventDiner,
+	getEventGuardian,
+} from "../../../trpc/trpc";
 
 export default defineEventHandler(async (event) => {
-	const catererStaff = await getEventCatererStaff(event);
-	if (!catererStaff)
-		throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
-
 	const id = Number(getRouterParam(event, "id"));
 	if (!Number.isFinite(id) || id <= 0)
 		throw createError({ statusCode: 400, statusMessage: "Invalid id" });
 
+	const [catererStaff, orgStaff, diner, guardian] = await Promise.all([
+		getEventCatererStaff(event),
+		getEventOrgStaff(event),
+		getEventDiner(event),
+		getEventGuardian(event),
+	]);
+
+	let dishWhere: object | null = null;
+
+	if (catererStaff) {
+		dishWhere = { id, caterer_id: catererStaff.caterer_id };
+	} else if (orgStaff) {
+		dishWhere = {
+			id,
+			Caterer: {
+				Contracts: { some: { organization_id: orgStaff.organization_id } },
+			},
+		};
+	} else if (diner) {
+		dishWhere = {
+			id,
+			Caterer: {
+				Contracts: { some: { organization_id: diner.organization_id } },
+			},
+		};
+	} else if (guardian) {
+		dishWhere = {
+			id,
+			Caterer: {
+				Contracts: {
+					some: {
+						Organization: {
+							Diners: {
+								some: { Guardians: { some: { guardian_id: guardian.id } } },
+							},
+						},
+					},
+				},
+			},
+		};
+	} else {
+		throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
+	}
+
 	const dish = await readClient.dish.findFirst({
-		where: { id, caterer_id: catererStaff.caterer_id },
+		where: dishWhere,
 		select: { Photo: { select: { path: true, type: true } } },
 	});
 	if (!dish?.Photo)
